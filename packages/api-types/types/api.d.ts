9dbf2b164cc812f160604d80f544d5b270e6c25e
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-22 13:29:47 +0530

    show pipelines on the pipelines page

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -482,6 +482,8 @@ export interface paths {
   '/platform/organizations/{slug}/tax-ids': {
     /** Gets the given organization's tax ID */
     get: operations['TaxIdsController_getTaxId']
+    /** Gets the given organization's tax ID */
+    get: operations['TaxIdsController_getTaxId']
     /** Creates or updates a tax ID for the given organization */
     put: operations['TaxIdsController_updateTaxId']
     /** Delete the tax ID with the given ID */
@@ -1004,6 +1006,16 @@ export interface paths {
      * @deprecated
      */
     get: operations['PropsSettingsController_getProjectApi']
+  }
+  '/platform/replication/{ref}/pipelines': {
+    /** Gets replication pipelines */
+    get: operations['ReplicationPipelinesController_getPipelines']
+    /** Creates a pipeline */
+    post: operations['ReplicationPipelinesController_createPipeline']
+  }
+  '/platform/replication/{ref}/pipelines/{pipeline_id}': {
+    /** Deletes a pipeline */
+    delete: operations['ReplicationPipelinesController_deletePipeline']
   }
   '/platform/replication/{ref}/sinks': {
     /** Gets replication sinks */
@@ -3011,6 +3023,16 @@ export interface components {
       publish_truncate?: boolean
       publish_update?: boolean
       tables?: string[] | null
+    }
+    CreateReplicationPipelineBody: {
+      /** @description Pipeline config */
+      config: components['schemas']['ReplicationPipelineConfig']
+      /** @description Publication name */
+      publication_name: string
+      /** @description Sink id */
+      sink_id: number
+      /** @description Source id */
+      source_id: number
     }
     CreateReplicationPublicationBody: {
       /** @description Publication name */
@@ -5090,10 +5112,26 @@ export interface components {
     RemoveReadReplicaBody: {
       database_identifier: string
     }
+    ReplicationBatchConfig: {
+      max_fill_secs: number
+      max_size: number
+    }
     ReplicationBigQueryConfig: {
       dataset_id: string
       project_id: string
       service_account_key: string
+    }
+    ReplicationPipelineConfig: {
+      config: components['schemas']['ReplicationBatchConfig']
+    }
+    ReplicationPipelinesResponse: {
+      config: components['schemas']['ReplicationPipelineConfig']
+      id: number
+      publication_name: string
+      replicator_id: number
+      sink_id: number
+      source_id: number
+      tenant_id: string
     }
     ReplicationPostgresConfig: {
       host: string
@@ -5727,6 +5765,16 @@ export interface components {
     }
     TaxIdResponse: {
       tax_id: components['schemas']['TaxId'] | null
+    }
+    TelemetryEventBody: {
+      action: string
+      category: string
+      ga?: components['schemas']['GoogleAnalyticBody']
+      label?: Record<string, never>
+      page_location?: string
+      page_referrer?: string
+      page_title?: string
+      value?: string
     }
     TelemetryEventBodyV2: {
       action: string
@@ -9531,6 +9579,8 @@ export interface operations {
   }
   /** Gets the given organization's tax ID */
   TaxIdsController_getTaxId: {
+  /** Gets the given organization's tax ID */
+  TaxIdsController_getTaxId: {
     parameters: {
       path: {
         /** @description Organization slug */
@@ -9546,6 +9596,7 @@ export interface operations {
       403: {
         content: never
       }
+      /** @description Failed to retrieve the organization's tax ID */
       /** @description Failed to retrieve the organization's tax ID */
       500: {
         content: never
@@ -9569,6 +9620,7 @@ export interface operations {
       200: {
         content: {
           'application/json': components['schemas']['TaxIdResponse']
+          'application/json': components['schemas']['TaxIdResponse']
         }
       }
       403: {
@@ -9589,6 +9641,7 @@ export interface operations {
       }
     }
     responses: {
+      204: {
       204: {
         content: never
       }
@@ -13394,6 +13447,69 @@ export interface operations {
         }
       }
       /** @description Failed to retrieve project's settings */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Gets replication pipelines */
+  ReplicationPipelinesController_getPipelines: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+      }
+    }
+    responses: {
+      200: {
+        content: {
+          'application/json': components['schemas']['ReplicationPipelinesResponse'][]
+        }
+      }
+      /** @description Failed to get replication pipeline */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Creates a pipeline */
+  ReplicationPipelinesController_createPipeline: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+      }
+    }
+    requestBody: {
+      content: {
+        'application/json': components['schemas']['CreateReplicationPipelineBody']
+      }
+    }
+    responses: {
+      201: {
+        content: never
+      }
+      /** @description Failed to create pipeline */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Deletes a pipeline */
+  ReplicationPipelinesController_deletePipeline: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Pipeline id */
+        pipeline_id: number
+      }
+    }
+    responses: {
+      200: {
+        content: never
+      }
+      /** @description Failed to delete pipelin */
       500: {
         content: never
       }
